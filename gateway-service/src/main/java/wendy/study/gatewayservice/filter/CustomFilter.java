e67a89e84f87a9ab5203806ca162f9b84eb31cb7
beeguriri <kkamboting@gmail.com>
2024-01-08 20:45:03 +0900

    안쓰는 import 삭제

--- gateway-service/src/main/java/wendy/study/gatewayservice/filter/CustomFilter.java
+++ gateway-service/src/main/java/wendy/study/gatewayservice/filter/CustomFilter.java
@@ -8,8 +8,6 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-
 @Component
 @Slf4j
 public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
