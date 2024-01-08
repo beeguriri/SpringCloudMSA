ae06da2d431011ed3b9a8a8b735ebb3a440d010a
beeguriri <kkamboting@gmail.com>
2024-01-08 21:30:40 +0900

    로드밸런싱 확인 - 라운드로빈

--- first-service/src/main/java/wendy/study/firstservice/FirstServiceController.java
+++ first-service/src/main/java/wendy/study/firstservice/FirstServiceController.java
@@ -1,15 +1,22 @@
 package wendy.study.firstservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
+@RequiredArgsConstructor
 public class FirstServiceController {
+
+    private final Environment env;
 
     @GetMapping("/hello")
     public String hello() {
@@ -23,7 +30,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "This is a message from First Service";
+    public String check(HttpServletRequest request) {
+        log.info("server port : {}", request.getServerPort());
+        return String.format("This is a message from First Service on Port [%s]", env.getProperty("local.server.port"));
     }
 }
