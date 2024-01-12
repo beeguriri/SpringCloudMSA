f2d12caaff0c9725ce35027f065d2347371dd2a7
beeguriri <kkamboting@gmail.com>
2024-01-13 07:30:33 +0900

    order service 개발 준비

---
+++ order-service/src/test/java/wendy/study/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package wendy.study.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
